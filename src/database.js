4215ecb48be6b5c49395b22b5974df6c70031f56
Luis Fernando Cruz <lf-cruz@hotmail.com>
2022-11-17 19:56:35 -0600

    make it better

--- src/database.js
+++ src/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/notasdb")
-    .then(db => console.log("Base de datos conectada"))
+const url = "mongodb+srv://notas:notas.2022@cluster0.h70hwng.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(url)
+    .then(console.log("Base de datos conectada"))
     .catch(error => console.log(error));
